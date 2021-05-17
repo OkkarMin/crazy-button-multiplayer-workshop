697ece9ba443daf7b304fd306aab39492e68b956
Okkar Min <omin001@e.ntu.edu.sg>
2021-05-17 20:32:38 +0800

    refactor: :bulb: Add comments to server.py

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+# import libraries that will help us with creating the server
 import socketio
 from aiohttp import web
 from random import uniform
@@ -17,30 +18,41 @@ playerList = {}
 
 
 # start of event handlers
+# a player has joined
 @sio.event
 async def connect(sid, _):
+    # add player to playerList with initial score of 0
     playerList[sid] = 0
 
+    # announce to everybody connected that a new player has connected
+    # with payload of playerList
     await sio.emit('player_connected', {'playerList': playerList})
 
 
+# a player has left
 @sio.event
 async def disconnect(sid):
     # remove player from playerList
     playerList.pop(sid)
 
+    # announce to everybody connected that a player has disconnected
+    # with payload of playerList
     await sio.emit('player_disconnected', {'playerList': playerList})
 
 
+# a player has pressed the button
 @sio.event
 async def button_pressed(sid):
-    # get randomTop, randonLeft a value between [0, 1]
+    # randomTop and randonLeft is value between [0, 1]
     randomTop = uniform(0, 1)
     randomLeft = uniform(0, 1)
 
-    # increase player score count
+    # increase player score by 1
     playerList[sid] += 1
 
+    # announce to everybody connected that a player has pressed the button
+    # with payload of randomTop, randomLeft to move button on all players screen
+    # and playerList to update the score on the player screen
     await sio.emit(
         'new_button_location', {
             'randomTop': randomTop,
