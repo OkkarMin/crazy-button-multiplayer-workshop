eb9d88b1f3c2d0b2d27ca1de4e137eb090b2178b
Okkar Min <omin001@e.ntu.edu.sg>
2021-05-17 20:17:30 +0800

    feat: :rocket: Initial Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Crazy Buton workshop for SCSE Freshman 2021
+
+## End product
+
+[insert gif of end product here]
+
+## How it works
+
+[insert image of architecture diagram]
+
+## Step 1
+
+## Step 2
+
+## Step 3
+
+## Step 4
---
+++ server.py
@@ -0,0 +1,66 @@
+import socketio
+from aiohttp import web
+from random import uniform
+
+# create a Socket.IO server
+sio = socketio.AsyncServer(cors_allowed_origins='*')
+app = web.Application()
+sio.attach(app)
+
+# keep track of list of connected players
+# {  player1_id: score,
+#    player2_id: score,
+#    player3_id: score,
+#    ...
+# }
+playerList = {}
+
+
+# start of event handlers
+@sio.event
+async def connect(sid, _):
+    playerList[sid] = {}
+    pass
+
+
+@sio.event
+async def new_player(sid, name):
+    # name + first 4 character of sid
+    name_sid = name + '--' + sid[0:2]
+
+    # add player to playerList initial score is 0
+    playerList[sid] = {'score': 0, 'name_sid': name_sid}
+
+    await sio.emit('player_connected', {'playerList': playerList})
+
+
+@sio.event
+async def disconnect(sid):
+    # remove player from playerList
+    playerList.pop(sid)
+
+    await sio.emit('player_disconnected', {'playerList': playerList})
+
+
+@sio.event
+async def button_pressed(sid):
+    # get randomTop, randonLeft a value between [0, 1]
+    randomTop = uniform(0, 1)
+    randomLeft = uniform(0, 1)
+
+    # increase player score count
+    playerList[sid]['score'] += 1
+
+    await sio.emit(
+        'new_button_location', {
+            'randomTop': randomTop,
+            'randomLeft': randomLeft,
+            'playerList': playerList,
+        })
+
+
+# end of event handlers
+
+# start server
+if __name__ == '__main__':
+    web.run_app(app)
